c6d0e125502aab751255b058eb47a0ab1fd66329
Ksenia Kovtonyuk <xeniakovtonyuk@yandex.ru>
2025-12-07 19:07:09 +0300

    С исправлениями

--- src/Actor.java
+++ src/Actor.java
@@ -1,5 +1,19 @@
+import java.util.Objects;
+
 public class Actor extends Person{
     int height;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Actor actor = (Actor) o;
+        return height == actor.height;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(height);
+    }
 
     public Actor(String name, Gender gender, String surname, Integer height) {
         super(name, gender, surname);
